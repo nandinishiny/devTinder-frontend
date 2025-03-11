81fc86c8f98e61df6ddde9897a87ac1bf3bd11a4
nandinishiny <nandinishiny136@gmail.com>
2025-03-11 14:36:54 +0530

    completed router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
   )
 }
 
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Error from "./components/Error";
+import Home from "./components/Home";
 
 const AppRouter = createBrowserRouter([
     {
         element: <App />,
         path: "/",
-    },{
-        path: "/about",
-        element: <div>about</div>
+        errorElement:<Error/>,
+        children:[
+            {
+                path: "/",
+                element:<Home/> 
+            },
+            {
+                path: "/about",
+                element: <div>about</div>
+            }
+
+        ]
     }
 ]);
 export default AppRouter;
